0a76361339e9c0f4ee2ee6612dd36860421b6cd0
Marc Dassonneville <marcd@zescore.com>
2013-05-19 23:17:13 +0200

    static server

--- server/server.js
+++ server/server.js
@@ -1,6 +1,14 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World !!\n');
-}).listen(80);
-console.log('Server running at http://*:80/');
+var fs = require('fs');
+
+if (fs.existsSync('../../yeswescore-server/server/conf.js')) {
+  // spawning server
+  var app = require('./app.js');
+  var Conf = require('../../yeswescore-server/server/conf.js');
+  app.listen(Conf.get('www.http.port'));
+} else {
+  // spawning blank error page
+  http.createServer(function (req, res) {
+    console.log('missing configuration file ' + confFile);
+    res.send(500, ''); // we do not give any info to the user
+  }).listen(80);
+}
