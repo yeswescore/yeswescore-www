199e2df5c517a402e97d681b237e67d5ee97003d
Marc Dassonneville <marcd@zescore.com>
2013-05-19 23:47:23 +0200

    demo of api call.

--- server/server.js
+++ server/server.js
@@ -1,9 +1,27 @@
-var fs = require('fs');
+var fs = require('fs'),
+  rest = require('restler');
 
 if (fs.existsSync('../../yeswescore-server/server/conf.js')) {
   // spawning server
   var app = require('./app.js');
   var Conf = require('../../yeswescore-server/server/conf.js');
+  
+  // dynamic pages
+  app.get('/test.html', function (req, res) {
+    // list of games
+    var host = Conf.get('http.host')
+      , port = Conf.get('http.port')
+      , api  = Conf.get('api.games');
+    // fetching yeswescore api
+    rest.get("http://"+host+":"+port+api).on('complete', function (games) {
+      // sending data to template
+      res.render('test.ejs', {
+        layout: false,
+        games: games
+      });
+    });
+  });
+  
   app.listen(Conf.get('www.http.port'));
 } else {
   // spawning blank error page
