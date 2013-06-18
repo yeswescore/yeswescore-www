ba8597a0963b73b26c43778a8b9234ad789624e1
Marc Dassonneville <marcd@zescore.com>
2013-06-19 01:03:24 +0200

    cache 4 jours pour www

--- server/pages.js
+++ server/pages.js
@@ -4,3 +4,21 @@ var app = require('./app.js');
 app.get('/', function (req, res) {
   res.sendfile('./public/build/index.html');
 });
+
+app.get('/:version/app.css', function (req, res) {
+  res.setHeader("Cache-Control", "public, max-age=345600"); // 4 days
+  res.setHeader("Expires", new Date(Date.now() + 345600000).toUTCString());
+  res.sendfile('./public/build/' + req.params.version + '/app.css');
+});
+
+app.get('/:version/app.js', function (req, res) {
+  res.setHeader("Cache-Control", "public, max-age=345600"); // 4 days
+  res.setHeader("Expires", new Date(Date.now() + 345600000).toUTCString());
+  res.sendfile('./public/build/' + req.params.version + '/app.js');
+});
+
+app.get('/:version/templates.html', function (req, res) {
+  res.setHeader("Cache-Control", "public, max-age=345600"); // 4 days
+  res.setHeader("Expires", new Date(Date.now() + 345600000).toUTCString());
+  res.sendfile('./public/build/' + req.params.version + '/templates.html');
+});
