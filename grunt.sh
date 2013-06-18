44f4521ac00f13fb81a1287667c0f40c9768bd1e
Marc Dassonneville <marcd@zescore.com>
2013-06-18 23:09:03 +0200

    grunt avec versionning

---
+++ grunt.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+fileversion=/tmp/yeswescore-www-build-version
+
+# fichier de version inexistant
+if [ ! -f $fileversion ]
+then
+  echo "WARNING: fichier de version inexistant, la version utilisée sera [1]"
+  echo "continuer ? [Y/n]"
+  read o
+  if [ $o != "Y" ]
+  then
+    echo "bye bye"
+    exit 1
+  fi
+  echo "1" > $fileversion
+fi
+
+# lecture de la version actuelle
+oldversion=`cat $fileversion`
+version=`expr $oldversion + 1`
+
+# on remplace la version par la nouvelle
+echo $version > $fileversion
+
+# petit message
+echo "grunt.sh: Version utilisee pour la build = "$version
+
+# export env var
+export YESWESCORE_WWW_BUILD_VERSION=$version
+
+# on build
+grunt --force web
