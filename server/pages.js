5c286d8cb6fa35f68b3ea74122b2778d8c561e9b
Marc Dassonneville <marcd@zescore.com>
2013-06-19 22:56:06 +0200

    security fix + versioning images

--- server/pages.js
+++ server/pages.js
@@ -26,5 +26,19 @@ app.get('/:version/app.js', function (req, res) {
 app.get('/:version/templates.html', function (req, res) {
   res.setHeader("Cache-Control", "public, max-age=345600"); // 4 days
   res.setHeader("Expires", new Date(Date.now() + 345600000).toUTCString());
-  res.sendfile('./public/build/' + req.params.version + '/templates.html');
+  var version = parseInt(req.params.version, 10);
+  if (version)
+    return res.sendfile('./public/build/' + version + '/templates.html');
+  res.send(404, "page not found");
+});
+
+// cas particulier des images
+// FIXME: security, check :name
+app.get('/:version/images/:name.jpg', function (req, res) {
+  res.setHeader("Cache-Control", "public, max-age=345600"); // 4 days
+  res.setHeader("Expires", new Date(Date.now() + 345600000).toUTCString());
+  var version = parseInt(req.params.version, 10);
+  if (version)
+    return res.sendfile('./public/images/' + req.params.name + '.jpg');
+  res.send(404, "page not found");
 });
