70f4a178fbc1b3aa6d4241c89d2afc97ff797110
Vincent <vincent@zescore.com>
2013-07-26 17:29:45 +0200

    site add og-fb et title

--- server/pages.js
+++ server/pages.js
@@ -1,4 +1,5 @@
 var app = require('./app.js'),
+   restify = require('restify'),
    Conf = require('../../yeswescore-server/server/conf.js');
 
 var env = Conf.get("env");
@@ -6,42 +7,71 @@ var index = (env === "DEV") ? '../private/index.html' : 'build/index.html';
 
 // HOME
 app.get('/', function (req, res) {
-  res.render(index, {foo: "bar"});
+  var urlsite = req.protocol + "://" + req.get('host') + req.url;
+  res.render(index, {title: "YesWeScore",url:urlsite});
 });
 
 // about
 app.get('/about/', function (req, res) {
-  res.render('../public/blog.html', {foo: "bar"});
+  var urlsite = req.protocol + "://" + req.get('host') + req.url;
+  res.render('../public/blog.html', {title: "YesWeScore",url:urlsite});
 });
 
 // resultats de recherche
 app.get('/clubs/list/:txt', function (req, res) {
-  res.render(index, {foo: "bar"});
+  var urlsite = req.protocol + "://" + req.get('host') + req.url;
+  res.render(index, {title: "YesWeScore",url:urlsite});
 });
 
 // page game (avec club)
 app.get('/games/:id/club/:clubid', function (req, res) {
-  res.render(index, {foo: "bar"});
+  var urlsite = req.protocol + "://" + req.get('host') + req.url;
+  res.render(index, {title: "YesWeScore",url:urlsite});
 });
 
 // page game
 app.get('/games/:id', function (req, res) {
-  res.render(index, {foo: "bar"});
+
+  var urlsite = req.protocol + "://" + req.get('host') + req.url;
+  var url = "http://"+Conf.get("http.host")+':'+Conf.get("http.port");
+
+  var client = restify.createJsonClient({
+    url: url
+  });
+
+  client.get(url+Conf.get("api.games")+req.params.id, function (err, req2, res2, game) {
+    if (err) {
+      //console.log('url',url+Conf.get("api.games")+req.params.id);
+      console.log(err);
+    }
+      
+    //console.log('Server returned: %j', game);
+    
+    //TODO : on construit le message
+    // Vincent T (15/5) contre Antoine (15/3) à Calais le 24/09 à 17:10
+    
+  	res.render(index, {title: "Suivez le match "+game.teams[0].players[0].name+" vs "+game.teams[1].players[0].name,url:urlsite});  
+  });
+
+  
 });
 
 // page club
 app.get('/clubs/:id', function (req, res) {
-  res.render(index, {foo: "bar"});
+  var urlsite = req.protocol + "://" + req.get('host') + req.url;
+  res.render(index, {title: "YesWeScore",url:urlsite});
 });
 
 // page player/signin
 app.get('/players/signin', function (req, res) {
-  res.render(index, {foo: "bar"});
+  var urlsite = req.protocol + "://" + req.get('host') + req.url;
+  res.render(index, {title: "YesWeScore",url:urlsite});
 });
 
 // page players/profil
 app.get('/players/profil', function (req, res) {
-  res.render(index, {foo: "bar"});
+  var urlsite = req.protocol + "://" + req.get('host') + req.url;
+  res.render(index, {title: "YesWeScore",url:urlsite});
 });
 
 app.get('/:version/app.css', function (req, res) {
